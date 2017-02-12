e4e2ac97a779867671d5e3863c0137c1d40f1163
Glenn Vandeuren <vandeurenglenn@gmail.com>
2017-02-12 01:27:04 +0100

    :metal: Initial PubSub support (1/2)

--- src/custom-template-if.js
+++ src/custom-template-if.js
@@ -9,28 +9,55 @@ export default class CustomTemplateIf extends HTMLElement {
   }
 
   set if(value) {
-    let boolean;
-    if (value === 'null' || value === 'false' || value === '0' || value === 'undefined') {
-       boolean = Boolean(false);
-    } else {
-      boolean = Boolean(true);
-    }
-
-    if (this._needsRender(boolean))
-      this._if = boolean;
-      this.render();
+    if (this._needsRender(this._booleanIf(value)))
+      if (!this.pubsub) {
+        this._if = value;
+        this.render();
+      } else {
+        if (!this._if) {
+          this._if = false;
+        }
+        PubSub.subscribe(value, function() {
+            this._if = true
+            this.render();
+        }.bind(this));
+      }
   }
 
   get if() {
     return this._if || false;
   }
 
-  get _child() {
-    return this.querySelector('template').children[0];
+  get pubsub() {
+    return this._pubsub || this.hasAttribute('pubsub');
+  }
+
+  set pubsub(value) {
+    this._pubsub = value;
+  }
+
+  get template() {
+    const template = this.children[0];
+    if (template === null ||
+        template === undefined ||
+        template.tagName !== 'TEMPLATE')
+      return console.warn('template not found');
+    else
+      return this.children[0];
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
     if (oldValue !== newValue) this[name] = newValue;
+  }
+
+  _booleanIf(value) {
+    let boolean;
+    if (value === 'null' || value === 'false' || value === '0' || value === 'undefined') {
+       boolean = Boolean(false);
+    } else {
+      boolean = Boolean(true);
+    }
+    return boolean;
   }
 
   /**
@@ -38,19 +65,21 @@ export default class CustomTemplateIf extends HTMLElement {
    * @param {string|boolean|number|object|array} value
    */
   _needsRender(value) {
-    if (value === this.if) return false;
+    if (value === this._booleanIf(this.if) && this.renderedOnce) return false;
     return true;
   }
 
   render() {
-    let f = this.if;
-    let child = this.child;
-    if (child === null || child === undefined)
-      return console.warn('template not found');
-    if (f !== false || f !== null || f !== undefined || f !== 0)
-      this._updateDom('append', child);
-    else this._updateDom('remove', child);
+    let f = this._booleanIf(this.if);
+    if (f === true)
+      this._updateDom('append', this.template.innerHTML);
+    else
+      for (let child of this.root.children)
+        this._updateDom('remove', this.child);
+
     this._fireEvent(f);
+
+    if (!this.renderedOnce) this.renderedOnce = true;
   }
 
   /**
@@ -58,8 +87,11 @@ export default class CustomTemplateIf extends HTMLElement {
    * @param {string} method 'append' or 'remove'
    * @param {HTMLElement} template
    */
-  _updateDom(method, child) {
-    this.root[method + 'Child'](child);
+  _updateDom(method, template) {
+    if (method === 'append')
+      this.root.innerHTML += template;
+    else
+      this.root.innerHTML -= template;
   }
 
   _fireEvent(detail) {
